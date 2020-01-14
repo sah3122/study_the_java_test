61b5b1a83aefc2c52558298c46b758092900ff92
dongchul <sah3122@naver.com>
2020-01-14 13:01:58 +0900

    Add TestRepetition Part 1

--- src/test/java/me/study/thejavatest/StudyTest.java
+++ src/test/java/me/study/thejavatest/StudyTest.java
@@ -2,6 +2,8 @@ package me.study.thejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 import java.util.function.Supplier;
@@ -80,6 +82,19 @@ class StudyTest {
 
         Study actual = new Study(10);
         assertThat(actual.getLimit()).isGreaterThan(0);
+    }
+
+    @DisplayName("스터디 만들기 10회 반복")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition} / {totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("스터디 만들기 파라미터")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+    void parameterizedTest(String message) {
+        System.out.println(message);
     }
 
     @BeforeAll
